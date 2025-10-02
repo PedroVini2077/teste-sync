84d0f3401cc484f42f6378908b08e0085ad2f657
PedroVini2077 <pedrovinicioscorrea@gmail.com>
2025-10-01 21:58:15 -0300

    Update 2025-10-01 21:58

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/python3
 
 nome = input("coloque o seu nome: ")
-print(f"Olá, {nome}! seja bem vindo!!")
+idade = input("coloque a sua idade: ")
+
+
+print(f"Olá, {nome}! seja bem vindo!! Vc tem {idade} anos, vc tem acesso a todo o site.")
+
+
